e985b1dba3cf29ee1e4cba86f320b1958288e45e
Bryan Kuiper <b.kuiper@live.nl>
2019-04-22 16:11:46 +0200

    init

--- src/ie-component.vue
+++ src/ie-component.vue
@@ -1,20 +1,35 @@
 <template>
     <div>
+        <fixed-header>
+            <div class="navbar">
+                Menu
+            </div>
+        </fixed-header>
         <span>This is the main component</span>
         <sub-component />
     </div>
 </template>
 
 <script>
-import SubComponent from "./components/sub-component.vue"
+    import FixedHeader from "vue-fixed-header";
+    import SubComponent from "./components/sub-component.vue"
 
-export default {
+    export default {
 
-    name: "ie-component",
-    data( ){
-        return { }
-    },
-    components: { SubComponent },
-    
-}
+        name: "ie-component",
+        data( ){
+            return { }
+        },
+        components: { FixedHeader, SubComponent },
+        
+    }
 </script>
+
+<style>
+    .navbar.vue-fixed-header--isFixed {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100vw;
+    }
+</style>
